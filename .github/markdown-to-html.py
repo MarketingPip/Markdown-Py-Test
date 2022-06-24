a3176120ef9134d3b3e2c596b2566511509ada67
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-24 09:25:06 -0400

    Update markdown-to-html.py

--- .github/markdown-to-html.py
+++ .github/markdown-to-html.py
@@ -113,17 +113,17 @@ else:
 # Write the file out that we have created
 try:
     with codecs.open(output_file, 'w', encoding='utf-8') as f:
-        f.write(f"""<head><title>{SiteTitle}
+        f.write(f"""<head><title>{SiteTitle}</title>
             <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>{SiteTitle}</title>
+
 <script src="https://cdn.jsdelivr.net/npm/prismjs@1.28.0/prism.min.js">
 {Facebook_Meta}         
 
 
-        </title>""" + """
+     </head>""" + """
 	<style>
 	
 	body { 
