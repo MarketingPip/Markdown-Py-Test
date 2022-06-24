819a4a009c63f0190aea38bf903a1fe01f6004ff
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-24 03:29:51 -0400

    Update markdown-to-html.py

--- .github/markdown-to-html.py
+++ .github/markdown-to-html.py
@@ -1,5 +1,5 @@
 import requests
 
-with open("test.md", "r") as markdown, open("index.html", "w") as html:
+with open("README.md", "r") as markdown, open("index.html", "w") as html:
     payload = {"text": markdown.read(), "mode": "markdown"}
     html.write(requests.post("https://api.github.com/markdown", json=payload).text)
