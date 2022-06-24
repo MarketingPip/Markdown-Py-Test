710524958bf5fc0497e100ff6ce07a31546c896a
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-24 10:30:16 -0400

    Stop splitting before line

--- .github/markdown-to-html.py
+++ .github/markdown-to-html.py
@@ -43,7 +43,7 @@ var = {}
 with open(input_file) as conf:
         for line in conf:
                 if ":" in line:
-                        name, value = line.split(":")  # Needs replaced with regex match 
+                        name, value = line.split('=================END OF SEO SETTINGS============')[0].split(':')  # Needs replaced with regex match 
                         var[name] = str(value).rstrip() # needs a value added
                         
 globals().update(var)
