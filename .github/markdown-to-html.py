d14edd1c6fe48fceecd4ac7820ac4e9c5c353777
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-06-24 07:34:22 -0400

    Fix

--- .github/markdown-to-html.py
+++ .github/markdown-to-html.py
@@ -45,8 +45,8 @@ with open(input_file) as conf:
                 if ":" in line:
                         name, value = line.split(":")  # Needs replaced with regex match 
                         var[name] = str(value).rstrip() # needs a value added
-                        date, value = line.split("==")  # Needs replaced with regex match 
-                        var[date] = str(value).rstrip() # needs a value added
+                        data, value2 = line.split("=") 
+                        var[data] = str(value2).rstrip()
 globals().update(var)
 
     
